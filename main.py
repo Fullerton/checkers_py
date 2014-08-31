44f9b7a6416a3c517a087f741d3219f821aab2cc
artv3 <artv3.89@gmail.com>
2014-08-31 10:35:07 -0500

    added main

---
+++ main.py
@@ -0,0 +1,18 @@
+#Main source code:
+#
+
+#What do we need for this? 
+#We need an a Board
+#and peices
+
+#Should we try an OO approach? where the board is an object
+#the pieces should also be members of the class? or should they be their own?
+
+#of course we need to visualize this? Any ideas? 
+#No need to get fancy, I think numpy and scipy will suffice for our visualizing needs. 
+def main():
+
+    print "checkers comming soon...."
+
+
+main()
